495e9f2e3e8f1216a013cb9a989c472cbe76e7f6
Andrey Burakov <andjeybur@gmail.com>
2025-01-24 00:28:44 +0300

    feat: disconnect wallet

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,8 @@
-import { TonConnectButton, useTonConnectModal } from '@tonconnect/ui-react'
+import {
+  useTonConnectModal,
+  useTonConnectUI,
+  useTonWallet,
+} from '@tonconnect/ui-react'
 import { Wallet2 } from 'lucide-react'
 import toast from 'react-hot-toast'
 
@@ -6,20 +10,38 @@ import { Button } from '../ui/button'
 
 export const Header = () => {
   const { close, open, state } = useTonConnectModal()
+  const wallet = useTonWallet()
+  const [tonConnectUI] = useTonConnectUI()
+
+  console.info({ wallet })
 
   const handleWalletConnect = () => {
     toast.success('Wallet connected successfully')
+  }
+
+  const handleWalletDisconnect = () => {
+    toast.promise(tonConnectUI.disconnect(), {
+      loading: 'Disconnecting...',
+      success: 'Wallet disconnected successfully',
+      error: 'Error while disconnecting. Please try again',
+    })
   }
 
   return (
     <header className="sticky z-50 top-0 bg-slate-900/80 backdrop-blur-sm p-4 border-b border-slate-700">
       <div className="max-w-md mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">Subscription Hub</h1>
-        {/* <TonConnectButton className="bg-red" /> */}
-        <Button onClick={open}>
-          <Wallet2 className="w-4 h-4" />
-          <span>Connect Wallet</span>
-        </Button>
+        {wallet ? (
+          <Button onClick={handleWalletDisconnect}>
+            <Wallet2 className="w-4 h-4" />
+            <span>Disconnect Wallet</span>
+          </Button>
+        ) : (
+          <Button onClick={open}>
+            <Wallet2 className="w-4 h-4" />
+            <span>Connect Wallet</span>
+          </Button>
+        )}
       </div>
     </header>
   )
