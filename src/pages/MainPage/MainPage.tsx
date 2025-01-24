7f26d0ac43a9cc77c332814232576a614e3d38a5
Andrey Burakov <andjeybur@gmail.com>
2025-01-24 14:27:20 +0300

    feat: add cloudstorage to save data between devices

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,5 @@
+import { cloudStorage, init } from '@telegram-apps/sdk'
+import { useLayoutEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 
 import { Header } from '@/components/header/header'
@@ -7,16 +9,76 @@ import { Button } from '@/components/ui/button'
 import { AlertsBlock } from './components/alerts-block'
 import { Card } from './components/card'
 
+const CARDS_DATA = [
+  {
+    id: 1,
+    title: 'Support Kirill Malev',
+    subtitle: '$1 daily contribution',
+    amount: 1,
+    subscribeBtnText: 'Subscribe $1/day',
+    btnColor: 'redGradient',
+  },
+  {
+    id: 2,
+    title: 'Support UNICEF Daily',
+    subtitle: '$1 daily for children in need',
+    amount: 1,
+    subscribeBtnText: 'UNICEF - $1/day',
+    btnColor: 'blueGradient',
+  },
+  {
+    id: 3,
+    title: 'Support UNICEF Monthly',
+    subtitle: '$30 monthly commitment',
+    amount: 30,
+    subscribeBtnText: 'Subscribe $30/month',
+    btnColor: 'blueGradient',
+  },
+] as const
+
 export const MainPage = () => {
-  const handleSubscribe = (plan: string) => {
-    console.info(`Subscribing to: ${plan}`)
-    toast.success(`Subscribed to: ${plan}`)
+  const [activeCards, setActiveCards] = useState<number[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useLayoutEffect(() => {
+    init()
+    cloudStorage.getItem('activeCards').then(data => {
+      console.info(data)
+      if (data) {
+        console.info(JSON.parse(data))
+        setActiveCards(JSON.parse(data))
+        setIsLoading(false)
+      }
+    })
+  }, [])
+
+  const handleSubscribe = (cardId: number) => {
+    const newActiveCards = [...activeCards, cardId]
+    setActiveCards(newActiveCards)
+    cloudStorage.setItem('activeCards', JSON.stringify(newActiveCards))
+
+    console.info(
+      `Subscribed to ${CARDS_DATA.find(card => card.id === cardId)?.title}`
+    )
+    toast.success(
+      `Subscribed to ${CARDS_DATA.find(card => card.id === cardId)?.title}`
+    )
   }
 
-  const handleUnsubscribe = (plan: string) => {
-    console.info(`Unsubscribing from: ${plan}`)
-    toast.success(`Unsubscribing to: ${plan}`)
+  const handleUnsubscribe = (cardId: number) => {
+    const newActiveCards = activeCards.filter(card => card !== cardId)
+    setActiveCards(newActiveCards)
+    cloudStorage.setItem('activeCards', JSON.stringify(newActiveCards))
+
+    console.info(
+      `Unsubscribed to ${CARDS_DATA.find(card => card.id === cardId)?.title}`
+    )
+    toast.success(
+      `Unsubscribed to ${CARDS_DATA.find(card => card.id === cardId)?.title}`
+    )
   }
+
+  console.info({ activeCards }, activeCards.includes(1))
 
   return (
     <div className="bg-gradient-to-b from-slate-900 to-slate-800 text-white">
@@ -24,92 +86,50 @@ export const MainPage = () => {
 
       <main className="max-w-md mx-auto p-4 space-y-6 pt-6">
         <div className="space-y-4">
-          <Card
-            title={'Support Kirill Malev'}
-            subtitle={'$1 daily contribution'}
-            actions={
-              <div className="space-y-2">
-                <ConfirmDialog
-                  title={
-                    'Are you sure you want to subscribe to Kirill Malev - $1/day?'
-                  }
-                  trigger={
-                    <Button
-                      variant={'redGradient'}
-                      size={'md'}
-                    >
-                      Subscribe $1/day
-                    </Button>
-                  }
-                  action={() => handleSubscribe('Kirill Malev - $1/day')}
-                />
+          {CARDS_DATA.map(card => {
+            return (
+              <Card
+                key={card.id}
+                title={card.title}
+                subtitle={card.subtitle}
+                actions={
+                  !isLoading ? (
+                    <div className="space-y-2">
+                      <ConfirmDialog
+                        title={`Are you sure you want to subscribe to ${card.title}?`}
+                        trigger={
+                          <Button
+                            variant={card.btnColor}
+                            size={'md'}
+                            disabled={activeCards.includes(card.id)}
+                          >
+                            {card.subscribeBtnText}
+                          </Button>
+                        }
+                        action={() => handleSubscribe(card.id)}
+                      />
 
-                <ConfirmDialog
-                  title={
-                    'Are you sure you want to unsubscribe to Kirill Malev - $1/day?'
-                  }
-                  trigger={
-                    <Button
-                      variant={'redGradient'}
-                      size={'md'}
-                      disabled
-                    >
-                      Unsubscribe
-                    </Button>
-                  }
-                  action={() => handleUnsubscribe('Kirill Malev - $1/day')}
-                />
-              </div>
-            }
-          />
-
-          <Card
-            title={'Support UNICEF Daily'}
-            subtitle={'$1 daily for children in need'}
-            actions={
-              <div className="space-y-2">
-                <Button
-                  variant={'blueGradient'}
-                  size={'md'}
-                  onClick={() => handleSubscribe('UNICEF - $1/day')}
-                >
-                  Subscribe $1/day
-                </Button>
-                <Button
-                  variant={'blueGradient'}
-                  size={'md'}
-                  onClick={() => handleUnsubscribe('UNICEF - $1/day')}
-                  disabled
-                >
-                  Unsubscribe
-                </Button>
-              </div>
-            }
-          />
-
-          <Card
-            title={'Support UNICEF Monthly'}
-            subtitle={'$30 monthly commitment'}
-            actions={
-              <div className="space-y-2">
-                <Button
-                  variant={'blueGradient'}
-                  size={'md'}
-                  onClick={() => handleSubscribe('UNICEF - $30/month')}
-                >
-                  Subscribe $30/month
-                </Button>
-                <Button
-                  variant={'blueGradient'}
-                  size={'md'}
-                  onClick={() => handleUnsubscribe('UNICEF - $30/month')}
-                  disabled
-                >
-                  Unsubscribe
-                </Button>
-              </div>
-            }
-          />
+                      <ConfirmDialog
+                        title={`Are you sure you want to unsubscribe from ${card.title}?`}
+                        trigger={
+                          <Button
+                            variant={'redGradient'}
+                            size={'md'}
+                            disabled={!activeCards.includes(card.id)}
+                          >
+                            Unsubscribe
+                          </Button>
+                        }
+                        action={() => handleUnsubscribe(card.id)}
+                      />
+                    </div>
+                  ) : (
+                    <>Loading...</>
+                  )
+                }
+              />
+            )
+          })}
         </div>
 
         <AlertsBlock />
